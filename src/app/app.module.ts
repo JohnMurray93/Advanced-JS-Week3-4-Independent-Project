4d5483d6e0644efd026860ef80885c16c9c8aa9f
John Murray <jmurray.seattle@gmail.com>
2018-09-04 10:42:00 -0700

    home and blog component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { FooterComponent } from './footer/footer.component';
 import { ArticleComponent } from './article/article.component';
 import { BottomadComponent } from './bottomad/bottomad.component';
 import { BlogComponent } from './blog/blog.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
@@ -26,7 +27,8 @@ import { BlogComponent } from './blog/blog.component';
     FooterComponent,
     ArticleComponent,
     BottomadComponent,
-    BlogComponent
+    BlogComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,12 +1,17 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { BlogComponent }   from './blog/blog.component';
+import { BlogComponent } from './blog/blog.component';
+import { HomeComponent } from './home/home.component';
 
 const appRoutes: Routes = [
-  {
+    {
+    path: '',
+    component: HomeComponent
+    },
+    {
     path: 'blog',
     component: BlogComponent
-  },
+    },
  ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
